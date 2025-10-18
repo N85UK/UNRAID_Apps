55a7aa6db65f14098fb0d2cd43d7eab10a05662f
Paul <paul.mccann@n85.uk>
2025-10-18 17:55:38 +0100

    fix: remove MUI imports from main.jsx

--- Apps/UCG-Max-Webhook-Receiver/frontend/src/main.jsx
+++ Apps/UCG-Max-Webhook-Receiver/frontend/src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const theme = createTheme();
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
